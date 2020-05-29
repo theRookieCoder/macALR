00ce42e1b03cca5fa42c5834f1a8e50c427a13f3
theRookieCoder <60034030+theRookieCoder@users.noreply.github.com>
2020-05-29 22:02:47 +0800

    Delete macALRinstaller.sh

--- macALRinstaller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo This script will install macALR
-mkdir ~/bin
-cp ~/Downloads/macALR-master/macALR ~/bin/
-cp ~/Downloads/macALR-master/macALR.1 ~/.local/share/man
-chmod +x ~/bin/macALR
-echo Installation complete
